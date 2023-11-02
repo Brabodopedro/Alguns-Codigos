c27bb4a17740c70d5a4f87fe6f44b52348a641ed
Pedro Henrique Brito Obara <106891475+Brabodopedro@users.noreply.github.com>
2023-11-02 13:45:37 -0400

    Create funçao.cpp

---
+++ C/funçao.cpp
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+
+//funçao que caucula a media da funçao
+int media(int a, int b)
+
+int main()
+{
+	int nota1, nota2, medias;
+
+	//colocar a nota1
+	printf("Nota1 : ");
+	scanf("%d", &nota1);
+
+	//colocar a nota2
+	printf("NOta2 : ");
+	scanf("%d", &nota2);
+
+	//calcula a media das notas
+	medias = media(nota1, nota2)
+	printf("Sua media é : %d", &media)
+
+}
+
+
+int media(int a, int b)
+{
+	media = (a + b)/2
+
+	printf("%s\n", &media);
+}
